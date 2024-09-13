77fe3ffe6992917ba211bbe5dec6d39611c23031
evilandrex <chowjason1234@gmail.com>
2024-09-12 20:51:39 -0700

    add encoding to ensure robustness

--- backend/constants/skill_info.py
+++ backend/constants/skill_info.py
@@ -3,9 +3,11 @@ from importlib import resources
 from . import jsons
 
 # Load Skill.json
-with resources.files(jsons).joinpath("Skill.json").open() as file:
+with resources.files(jsons).joinpath("Skill.json").open("r", encoding="utf-8") as file:
     skills_dict = json.load(file)
 
 # Load SkillBuff.json
-with resources.files(jsons).joinpath("SkillBuff.json").open() as file:
+with resources.files(jsons).joinpath("SkillBuff.json").open(
+    "r", encoding="utf-8"
+) as file:
     buffs_dict = json.load(file)
