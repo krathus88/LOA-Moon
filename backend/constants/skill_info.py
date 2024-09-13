73f7e6ba684b30e7b146822e73c45488e423e681
evilandrex <chowjason1234@gmail.com>
2024-09-12 20:45:27 -0700

    update symbol names

--- backend/constants/skill_info.py
+++ backend/constants/skill_info.py
@@ -4,8 +4,8 @@ from . import jsons
 
 # Load Skill.json
 with resources.files(jsons).joinpath("Skill.json").open() as file:
-    skills = json.load(file)
+    skills_dict = json.load(file)
 
 # Load SkillBuff.json
 with resources.files(jsons).joinpath("SkillBuff.json").open() as file:
-    buffs = json.load(file)
+    buffs_dict = json.load(file)
